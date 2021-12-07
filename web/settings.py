c7c26094967e41915653f7662ae98d4def5b75b0
William Ribeiro <rybeyro93@gmail.com>
2021-12-07 20:38:31 -0300

    add django_on_heroku call method in your past place

--- web/settings.py
+++ web/settings.py
@@ -126,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
