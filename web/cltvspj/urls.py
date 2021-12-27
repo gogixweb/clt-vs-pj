1cce00ab5e040fd002a53db40095e57b3e85283b
Eduardo Machado Rezende <eduardomachadorezende0@gmail.com>
2021-12-27 10:28:15 -0300

    Solving conflict

--- web/cltvspj/urls.py
+++ web/cltvspj/urls.py
@@ -9,5 +9,6 @@ api_router.register("calculator", CalculatorViewSet)
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("contact", views.contact, name="contact"),
     path("api/v1/", include(api_router.urls)),
 ]
