9c6424137b34cd9fa95eab669f5e2135739474a7
Fernando <fernandom@pop-os.localdomain>
2022-02-21 21:17:48 -0300

    api 'fixed' now it returns Json, but needs to pass parameter safe='false' needs to remove it as i dont think it is safe, also need to review the logic as the values can be negative depending on the parameters passed

--- web/cltvspj/models.py
+++ web/cltvspj/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from django.db.models.fields import CharField, IntegerField
 
 # Create your models here.
 
 class Calculator(models.Model):
-    pj = models.IntegerField(blank=True, null=True)
-    clt = models.IntegerField(blank=True, null=True)
+    pj = models.TextField(blank=True, null=True)
+    clt = models.TextField(blank=True, null=True)
 
-class Email(models.Model):
-    address = models.CharField(max_length=100, blank=True)
-    subject = models.CharField(max_length = 200, blank=True)
-    content = models.CharField(max_length = 800, blank=True)
+    def __str__(self):
+        return self.pj
